646ab1830f56ef25f0bd46e1b1616378e56f00bd
masaki12-s <asam.archer1112@yahoo.ne.jp>
2022-09-29 23:45:04 +0900

    linear search

--- chapter5/ALDS1_4_A/src/App.java
+++ chapter5/ALDS1_4_A/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int n = 5;
+        int[] S = {1,2,3,4,5};
+        int q = 3;
+        int[] T = {3,4,1};
+        int ans = Linear_Search(n, S, q, T);
+        System.out.println(ans);
+    }
+
+    public static int Linear_Search(int n, int[] S, int q, int[] T){
+        int C = 0;
+        for(int i=0; i<q; i++){
+            for(int j=0; j<n; j++){
+                if(T[i] == S[j]){
+                    C++;
+                }
+            }
+        }
+        return C;
     }
 }
